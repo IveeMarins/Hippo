10cc0b34f780494aab3def44c32a95d7d3b009bc
IveeMarins <iveemarins@gmail.com>
2015-05-01 18:46:33 -0300

    função deleteGoals terminada =)

--- HippoBank/GoalDAO.swift
+++ HippoBank/GoalDAO.swift
@@ -103,64 +103,29 @@ class GoalDAO {
         var numGoals: Int
         numGoals = self.savedInformation.integerForKey("NUM_GOALS")
         
-        /* exemplo: Tinhamos 4 goals, que estão salvos da seguinte forma:
-                Goal_0
-                Goal_1
-                Goal_2
-                Goal_3
-                
-           sopunhamos que vamos tirar o goal_1, vou enviar para a função o id dele, 1
-
-           precisamos repassar as informações dos proximos goals para a posição deste que foi retirado
-                
-         //somando 1 no id vamos pra proxima posição
-         position++
-                
-         */
-                
-          //rearrumando as keys a partir do proximo goal após o que foi retirado
-        
           position++
         
           for i = position; i < numGoals; i++ {
             
-            name = self.savedInformation.stringForKey("GOAL_NAME_\(i)")! //pegando info deste goal
-                    
-            self.savedInformation.setObject(name, forKey: "GOAL_NAME_\(i-1)") //pego o que estava neste goal e coloco na posição anterior
-                    
-            //realizo a mesma operação para todas as informações, price, moneySaved, priority, categoryType...
-                    
+            name = self.savedInformation.stringForKey("GOAL_NAME_\(i)")!
+            price = self.savedInformation.floatForKey("GOAL_PRICE_\(i)")
+            moneySaved = self.savedInformation.floatForKey("GOAL_SAVED_\(i)")
+            priority = self.savedInformation.integerForKey("GOAL_PRIORITY_\(i)")
+            categoryType = self.savedInformation.stringForKey("GOAL_CATEGORY_\(i)")!
+            
+            self.savedInformation.setObject(name, forKey: "GOAL_NAME_\(i-1)")
+            self.savedInformation.setFloat(price, forKey: "GOAL_PRICE_\(i-1)")
+            self.savedInformation.setFloat(moneySaved, forKey: "GOAL_SAVED_\(i-1)")
+            self.savedInformation.setInteger(priority, forKey: "GOAL_PRIORITY_\(i-1)")
+            self.savedInformation.setObject(categoryType, forKey: "GOAL_CATEGORY_\(i-1)")
+
             }
-        
-        /* voltando ao nosso exemplo:
-        Agora temos o seguinte
-        
-        Goal_0
-        Goal_1 ( com as informações que estavam em Goal_2 )
-        Goal_2 ( com as informações que estavam em Goal_3 )
-        Goal_3
-        
-        Tem alguma coisa errada neh? As infos que estavam em Goal_1 antes, sumiram, pois escrevemos em cima delas o que estava em Goal_2, porém o Goal_3 esta repetido
-        
-        Mas se diminuirmos 1 do numero total de goals
-        numGoals = numGoals - 1;
-        
-        e salvarmos isso: self.savedInformation.setInteger(numGoals, forKey: "NUM_GOALS")
-        
-        na proxima vez que formos ler iremos ignorar esse Goal_3. 
-        
-        Pronto, LINDO :D
-        */
         
         numGoals = numGoals - 1;
         self.savedInformation.setInteger(numGoals, forKey: "NUM_GOALS")
         
-        
-        //lendo a memória novamente para conseguirmos a lista sem o cara q deletamos
         goalsAfterDelete = self.searchGoals()
         
-        
-        // retorna a lista sem o que deletamos
         return goalsAfterDelete
     }
     
